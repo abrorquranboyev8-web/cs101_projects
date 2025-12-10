20d16dec152852c6a74622e27262dd06b822d2b9
Abrorbek <abrorquranboyev8@gmail.com>
2025-12-10 14:52:16 +0500

    Update week9assignment.py

--- week9assignment.py
+++ week9assignment.py
@@ -2,22 +2,19 @@ def audit_access(entries, min_risk):
     result = []
     for entry in entries:
         if entry != "":
-            # print(entry)
-            risk1 = entry.find("[") + 1
-            risk2 = entry.find("]")
-            risk = entry[risk1:risk2]
-            time = entry[6:11]
-            parts = entry.split(" | ")
-            if len(parts) >= 3:
-                zone = parts[1].strip()
-                event = parts[2].strip()
-                summary = f"{risk} ({time}): {zone} - {event}"
-                if min_risk == "LOW":
-                    result.append(summary)
-                elif min_risk == "MED" and (risk == "MED" or risk == "HIGH"):
-                    result.append(summary)
-                elif min_risk == "HIGH" and risk == "HIGH":
-                    result.append(summary)
+            left, right = entry.split("] ", 1)
+            risk = left[1:]
+            time = right[:5]
+            parts = right.split(" | ")
+            zone = parts[1].strip()
+            event = parts[2].strip()
+            summary = f"{risk} ({time}): {zone} - {event}"
+            if min_risk == "LOW":
+                result.append(summary)
+            elif min_risk == "MED" and (risk == "MED" or risk == "HIGH"):
+                result.append(summary)
+            elif min_risk == "HIGH" and risk == "HIGH":
+                result.append(summary)
     return result                
    
 
@@ -38,6 +35,3 @@ print(medium_and_high)
 
 high_only = audit_access(access_log, "HIGH")
 print(high_only)
-
-
-
